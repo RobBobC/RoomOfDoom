2a7a6972129e0a1ce4f236ef58fac2328eb8ee10
RobBobC <rjcook02@louisville.edu>
2015-03-27 13:04:01 -0400

    Movement, crosshair
    Character controller additions, crosshair mouse

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -3,13 +3,82 @@ using System.Collections;
 
 public class CharacterController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public float moveSpeed = 1.0f;
 	
+	//private Animator animator;
+	
+	private const int STATE_IDLE = 0;
+	private const int STATE_UP = 1;
+	private const int STATE_RIGHT = 2;
+	private const int STATE_DOWN = 3;
+	private const int STATE_LEFT = 4;
+	
+	private bool isIdle = true;
+	
+	private int currentAnimationState = STATE_IDLE;
+	
+	void Start()
+	{
+		//animator = this.GetComponent<Animator>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
+		if (Input.GetKey ("a") && Input.GetKey ("w")) {
+						transform.Translate (new Vector3 (-1, 1, 0) * moveSpeed * Time.deltaTime);
+				}
+		else if (Input.GetKey ("a") && Input.GetKey ("s")) {
+			transform.Translate (new Vector3 (-1, -1, 0) * moveSpeed * Time.deltaTime);
+		}
+		else if (Input.GetKey ("d") && Input.GetKey ("w")) {
+			transform.Translate (new Vector3 (1, 1, 0) * moveSpeed * Time.deltaTime);
+		}
+		else if (Input.GetKey ("d") && Input.GetKey ("s")) {
+			transform.Translate (new Vector3 (1, -1, 0) * moveSpeed * Time.deltaTime);
+		}
+			else if (Input.GetKey ("w")) { 
+					UpdateState (STATE_UP);
+					transform.Translate (Vector3.up * moveSpeed * Time.deltaTime);
+			} else if (Input.GetKey ("d")) {
+					UpdateState (STATE_RIGHT);
+					transform.Translate (Vector3.right * moveSpeed * Time.deltaTime);
+			} else if (Input.GetKey ("s")) {
+					UpdateState (STATE_DOWN);
+					transform.Translate (Vector3.down * moveSpeed * Time.deltaTime);	
+			} else if (Input.GetKey ("a")) {
+					UpdateState (STATE_LEFT);
+					transform.Translate (Vector3.left * moveSpeed * Time.deltaTime);
+			}
+			else if (isIdle)
+			{
+				//UpdateState(STATE_IDLE);
+			}
+	}
 	
+	void UpdateState(int state)
+	{
+		/*if (currentAnimationState == state)
+			return;
+		
+		switch (state)
+		{
+		case STATE_IDLE:
+			animator.SetInteger("PlayerState", STATE_IDLE);
+			break;
+		case STATE_UP:
+			animator.SetInteger("PlayerState", STATE_UP);
+			break;
+		case STATE_RIGHT:
+			animator.SetInteger("PlayerState", STATE_RIGHT);
+			break;
+		case STATE_DOWN:
+			animator.SetInteger("PlayerState", STATE_DOWN);
+			break;
+		case STATE_LEFT:
+			animator.SetInteger("PlayerState", STATE_LEFT);
+			break;
+		}
+		
+		currentAnimationState = state;*/
 	}
 }
---
+++ Assets/Scripts/CrosshairController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrosshairController : MonoBehaviour {
+
+	public Texture crosshairImage;
+
+	// Use this for initialization
+	void Start () {
+		Screen.showCursor = false;
+	}
+
+	/*void Update ()
+	{
+		transform.Translate(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+	}*/
+	
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect(Input.mousePosition.x - (crosshairImage.width / 2), (Screen.height - Input.mousePosition.y) - (crosshairImage.height / 2), crosshairImage.width, crosshairImage.height), crosshairImage);
+	}
+}
