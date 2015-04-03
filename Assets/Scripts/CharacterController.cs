b47356a10b6228cd2a4fb2042fed9c4aa21d26b5
Nindustrialist <nindustrialist@gmail.com>
2015-04-03 00:51:36 -0400

    Started Collision Detection Logic
    Logic is buggy, running into an object stops you from being able to get
    away from object

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -13,83 +13,84 @@ public class CharacterController : MonoBehaviour {
 	};
 
 	Animation currentAnimation;
-	CrosshairController crosshair;
-	
-	/*void AimAt ()
-	{
-		RaycastHit rayCastHit;
-		Vector3 vecLookToward;
-		Ray mouseRay = Camera.main.ScreenPointToRay (Input.mousePosition);
-		
-		if (Physics.Raycast (mouseRay, out rayCastHit, 150.0f)) {
-			vecLookToward = rayCastHit.point;
-		} else {
-			vecLookToward = mouseRay.origin + mouseRay.direction * 50.0f;
-		}
-		transform.LookAt (vecLookToward);
-	}
-
-	Animation ChangeAnimation (Animation anim)
-	{
-		if(anim == Animation.UP)
-		else if(direction == Direction.South)
-		else if(dir == Direction.East)
-		else if(dir == Direction.West)
-
-		return dir;     
-	}*/
+	bool blocked = false;
 
 	void Start()
 	{
-		crosshair = GameObject.FindGameObjectWithTag ("Crosshair").GetComponent<CrosshairController>();
 		animator = this.GetComponent<Animator>();
 		currentAnimation = Animation.IDLE;
 		UpdateAnimationState (currentAnimation);
 	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.tag == "Barrier" || collider.tag == "Enemy")
+		{
+			blocked = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D collider)
+	{
+		blocked = false;
+	}
 	
 	void Update()
 	{
-		if (Input.GetKey ("a") && Input.GetKey ("w")) {
-			UpdateAnimationState (Animation.WALK);
-			transform.Translate (new Vector3 (-1, 1, 0) * moveSpeed * Time.deltaTime);
-		} else if (Input.GetKey ("a") && Input.GetKey ("s")) {
-			UpdateAnimationState (Animation.WALK);
-			transform.Translate (new Vector3 (-1, -1, 0) * moveSpeed * Time.deltaTime);
-		} else if (Input.GetKey ("d") && Input.GetKey ("w")) {
-			UpdateAnimationState (Animation.WALK);
-			transform.Translate (new Vector3 (1, 1, 0) * moveSpeed * Time.deltaTime);
-		} else if (Input.GetKey ("d") && Input.GetKey ("s")) {
-			UpdateAnimationState (Animation.WALK);
-			transform.Translate (new Vector3 (1, -1, 0) * moveSpeed * Time.deltaTime);
-		} else if (Input.GetKey ("w")) { 
-			UpdateAnimationState (Animation.WALK);
-			transform.Translate (Vector3.up * moveSpeed * Time.deltaTime);
-		} else if (Input.GetKey ("s")) {
-			UpdateAnimationState (Animation.WALK);
-			transform.Translate (Vector3.down * moveSpeed * Time.deltaTime);	
-		} else if (Input.GetKey ("a")) {
-			UpdateAnimationState (Animation.WALK);
-			transform.Translate (Vector3.left * moveSpeed * Time.deltaTime);
-		} else if (Input.GetKey ("d")) {
-			UpdateAnimationState (Animation.WALK);
-			transform.Translate (Vector3.right * moveSpeed * Time.deltaTime);
-		}
-		else {
-			UpdateAnimationState (Animation.IDLE);
-		}
-
 		Vector3 moveToward = Camera.main.ScreenToWorldPoint( Input.mousePosition );
-		// 4
 		Vector3 moveDirection = moveToward - this.transform.position;
 		moveDirection.z = 0; 
 		moveDirection.Normalize();
 		
-		
 		float targetAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
-		transform.rotation = 
-			Quaternion.Slerp( transform.rotation, 
-			                 Quaternion.Euler( 0, 0, targetAngle - 90 ), 
-			                 5 * Time.deltaTime );
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler( 0, 0, targetAngle - 90 ), 5 * Time.deltaTime );
+		if(!blocked)
+		{
+			if (Input.GetKey ("a") && Input.GetKey ("w")) 
+			{
+				UpdateAnimationState (Animation.WALK);
+				transform.Translate (new Vector3 (-1, 1, 0) * moveSpeed * Time.deltaTime);
+			} 
+			else if (Input.GetKey ("a") && Input.GetKey ("s")) 
+			{
+				UpdateAnimationState (Animation.WALK);
+				transform.Translate (new Vector3 (-1, -1, 0) * moveSpeed * Time.deltaTime);
+			} 
+			else if (Input.GetKey ("d") && Input.GetKey ("w")) 
+			{
+				UpdateAnimationState (Animation.WALK);
+				transform.Translate (new Vector3 (1, 1, 0) * moveSpeed * Time.deltaTime);
+			} 
+			else if (Input.GetKey ("d") && Input.GetKey ("s")) 
+			{
+				UpdateAnimationState (Animation.WALK);
+				transform.Translate (new Vector3 (1, -1, 0) * moveSpeed * Time.deltaTime);
+			} 
+			else if (Input.GetKey ("w")) 
+			{ 
+				UpdateAnimationState (Animation.WALK);
+				transform.Translate (Vector3.up * moveSpeed * Time.deltaTime);
+			} 
+			else if (Input.GetKey ("s")) 
+			{
+				UpdateAnimationState (Animation.WALK);
+				transform.Translate (Vector3.down * moveSpeed * Time.deltaTime);	
+			} 
+			else if (Input.GetKey ("a")) 
+			{
+				UpdateAnimationState (Animation.WALK);
+				transform.Translate (Vector3.left * moveSpeed * Time.deltaTime);
+			} 
+			else if (Input.GetKey ("d")) 
+			{
+				UpdateAnimationState (Animation.WALK);
+				transform.Translate (Vector3.right * moveSpeed * Time.deltaTime);
+			}
+			else 
+			{
+				UpdateAnimationState (Animation.IDLE);
+			}
+		}
 	}
 	
 	void UpdateAnimationState(Animation curAnimState)
