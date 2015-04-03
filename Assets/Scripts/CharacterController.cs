5807f27f3a6bc2145f1af8f66a79245ad01ed8ef
RobBobC <rjcook02@louisville.edu>
2015-04-03 14:33:12 -0400

    Bullet Logic and Health
    Character can shoot bananas and be damaged.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,8 +4,14 @@ using System.Collections;
 public class CharacterController : MonoBehaviour {
 
 	public float moveSpeed = 1.0f;
+	public int health = 20;
+	public GameObject bullet;
+	public float shotSpeed = 1000;
 
 	private Animator animator;
+	private bool invincible;
+	private float invinceDuration;
+	private GameObject launchBox;
 
 	private enum Animation {
 		IDLE,
@@ -14,30 +20,6 @@ public class CharacterController : MonoBehaviour {
 
 	Animation currentAnimation;
 	CrosshairController crosshair;
-	
-	/*void AimAt ()
-	{
-		RaycastHit rayCastHit;
-		Vector3 vecLookToward;
-		Ray mouseRay = Camera.main.ScreenPointToRay (Input.mousePosition);
-		
-		if (Physics.Raycast (mouseRay, out rayCastHit, 150.0f)) {
-			vecLookToward = rayCastHit.point;
-		} else {
-			vecLookToward = mouseRay.origin + mouseRay.direction * 50.0f;
-		}
-		transform.LookAt (vecLookToward);
-	}
-
-	Animation ChangeAnimation (Animation anim)
-	{
-		if(anim == Animation.UP)
-		else if(direction == Direction.South)
-		else if(dir == Direction.East)
-		else if(dir == Direction.West)
-
-		return dir;     
-	}*/
 
 	void Start()
 	{
@@ -45,10 +27,25 @@ public class CharacterController : MonoBehaviour {
 		animator = this.GetComponent<Animator>();
 		currentAnimation = Animation.IDLE;
 		UpdateAnimationState (currentAnimation);
+		invincible = false;
+		invinceDuration = 1.0f;
+		launchBox = GameObject.FindGameObjectWithTag ("LaunchBox");
 	}
 	
 	void Update()
 	{
+		Debug.Log (health);
+		if(invincible)
+		{
+			if(invinceDuration < 0)
+			{
+				invincible = false;
+				invinceDuration = 1.0f;
+			}
+			else
+				invinceDuration -= Time.deltaTime;
+		}
+
 		if (Input.GetKey ("a") && Input.GetKey ("w")) {
 			UpdateAnimationState (Animation.WALK);
 			transform.Translate (new Vector3 (-1, 1, 0) * moveSpeed * Time.deltaTime);
@@ -90,6 +87,13 @@ public class CharacterController : MonoBehaviour {
 			Quaternion.Slerp( transform.rotation, 
 			                 Quaternion.Euler( 0, 0, targetAngle - 90 ), 
 			                 5 * Time.deltaTime );
+		
+		
+		if(Input.GetMouseButtonDown(0))
+		{
+			GameObject shot = Instantiate(bullet, launchBox.transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+			shot.rigidbody2D.AddForce(moveDirection*shotSpeed);
+		}
 	}
 	
 	void UpdateAnimationState(Animation curAnimState)
@@ -109,4 +113,33 @@ public class CharacterController : MonoBehaviour {
 		
 		currentAnimation = curAnimState;
 	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if(!invincible)
+		{
+			switch(other.collider.tag)
+			{
+				case "Rat":
+					health -= 1;
+					invincible = true;
+					break;
+			}
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D other)
+	{
+		if(!invincible)
+		{
+			switch(other.collider.tag)
+			{
+				case "Rat":
+					health -= 1;
+				    invincible = true;
+					break;
+			}		
+		}
+	}
+
 }
