d79023c0c5a599bb49dcf303cb7b2ee92f788227
RobBobC <rjcook02@louisville.edu>
2015-04-03 13:51:58 -0400

    Rat Movement
    Got rat movement/rotation working, fixed collisions

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,11 +4,6 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
     public GameObject player;
-    private Vector3 offset;
-	
-	void Start ()
-	{
-	}
 
 	void LateUpdate ()
 	{
---
+++ Assets/Scripts/RatController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RatController : BaseEnemy {
+	
+	private attackType attack;
+	private GameObject player;
+	private float lookAtAngle;
+
+	void Start ()
+	{
+		attack = attackType.melee;
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void Update ()
+	{
+		transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+		direction = player.transform.position - transform.position;
+		lookAtAngle = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis (lookAtAngle - 90, Vector3.forward);
+	}
+}
