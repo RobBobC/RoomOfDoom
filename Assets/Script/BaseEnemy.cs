d6d11264b5893f6f247afd789d7b2a1494db0da5
Zach Vilardell <zvilardell@gmail.com>
2015-04-06 16:36:18 -0400

    Enemy Inheritance, Bullet Damage, and Organization
    Updated BaseEnemy and RatController scripts.  BaseEnemy now handles all
    underlying animation/collision/health/death logic for enemies while
    RatController handles behavior specific to the rat enemy.  Controller
    scripts for future enemies will only need to define the behavior of that
    enemy (movement, projectiles, etc.).  Note that future enemy scripts
    should include a call to the base start function in Start() and check
    the IsDead property in the beginning of Update(), just like in the
    RatController.  The death animation transition for future enemies should
    also be based on an "isDead" boolean parameter, just like the rat's is.
    I also modified the BulletController script to include a "damagePoints"
    variable to store the hit damage information for the different
    projectile prefabs we'll have.  The axe currently does 1 damage, and a
    made a beer bullet for testing that does 2 damage (one hit kill for the
    rats).  This damagePoints value is used in the BaseEnemy controller to
    deduct the correct amount of health from an enemy when it is hit by a
    certain projectile.
    Lastly, I also organized the prefab folder into three sections for the
    three types of prefabs we currently have.

--- Assets/Script/BaseEnemy.cs
+++ Assets/Script/BaseEnemy.cs
@@ -5,10 +5,42 @@ public class BaseEnemy : MonoBehaviour {
 	public float moveSpeed;
 	public int health;
 
+	protected GameObject player;
+	protected Animator animator;
+	protected Vector3 direction;
+
 	protected enum attackType {
 		melee,
 		ranged
 	};
 
-	protected Vector3 direction;
+	protected bool IsDead //property to retrieve the isDead flag
+	{
+		get { return animator.GetBool("isDead"); }
+	}
+
+	protected void Start()
+	{
+		animator = this.GetComponent<Animator> ();
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	protected void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.collider.tag == "Bullet")
+		{
+			health -= other.collider.gameObject.GetComponent<BulletController>().damagePoints;
+			if(health <= 0)
+			{
+				animator.SetBool("isDead", true);
+				gameObject.collider2D.enabled = false;
+			}
+			Destroy (other.gameObject);
+		}
+	}
+
+	protected void GrantTheSweetReleaseOfDeath() //animation event
+	{
+		Destroy (gameObject);
+	}
 }
--- Assets/Script/BulletController.cs
+++ Assets/Script/BulletController.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class BulletController : MonoBehaviour {
 
+	public int damagePoints = 1;
+
 	void OnCollisionEnter2D(Collision2D other)
 	{
 		if(other.collider.tag == "Obstacle")
 			Destroy (gameObject);
 	}
-
 }
