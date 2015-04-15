9c44fda81d399339694224d67693ea808dd6c4b5
RobBobC <rjcook02@louisville.edu>
2015-04-15 02:19:03 -0400

    Sounds and GUIs
    GameController started as well

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	
+	public void StartButton ()
+	{
+		Application.LoadLevel (1);
+	}
+
+	public void MainMenu ()
+	{
+		Application.LoadLevel (0);
+	}
+}
