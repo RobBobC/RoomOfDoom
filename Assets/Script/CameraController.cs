1849feae1fd040a767d647691109fc82ddbad342
Zach Vilardell <zvilardell@gmail.com>
2015-04-14 21:34:56 -0400

    Fix Camera Clamping
    Camera clamping now handled dynamically based on the camera size and a
    bounding box around our level.  This should allow it to clamp correctly
    no matter what aspect ratio or screen resolution a player is playing
    with.

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -3,9 +3,37 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
     public GameObject player;
+	public BoxCollider2D cameraBounds;
+
+	private float halfCamWidth;
+	private float halfCamHeight;
+	private float x;
+	private float y;
+	private Vector3 min;
+	private Vector3 max;
+
+	void Start()
+	{
+		//the orthographic size is half of the camera view's height
+		halfCamHeight = camera.orthographicSize;
+		//calculate half of the camera view's width
+		halfCamWidth = halfCamHeight * ((float) Screen.width / Screen.height);
+
+		//max and min values are based on the bounds of the cameraBounds collider box area (disabled collision physics)
+		min = cameraBounds.bounds.min;
+		max = cameraBounds.bounds.max;
+	}
 
 	void LateUpdate ()
 	{
-		transform.position = new Vector3 (Mathf.Clamp(player.transform.position.x, -60, 64), Mathf.Clamp(player.transform.position.y, -77, 66), transform.position.z);
+		x = player.transform.position.x;
+		y = player.transform.position.y;
+
+		//ensure that the camera stays within its bounds
+		x = Mathf.Clamp(x, min.x + halfCamWidth, max.x - halfCamWidth);
+		y = Mathf.Clamp(y, min.y + halfCamHeight, max.y - halfCamHeight);
+
+		//update camera position
+		transform.position = new Vector3 (x, y, transform.position.z);
 	}
 }
